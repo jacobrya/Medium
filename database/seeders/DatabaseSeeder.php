9c4808156b370057937ae31e40c36db00b1f6611
jacobrya <zhan.nursadykov@gmail.com>
2026-01-16 02:31:43 +0500

    tried using foreach in blade templates and understood some debugging functions in PostController

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -16,6 +17,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::factory()->create([
+            'name' => 'John USers Doe',
+            'email' => 'test@example.com',
+
+        ]);
         $categories = [
             'Technology',
             'Health',
