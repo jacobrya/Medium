5e05432b554a7753e429dacc30f1b5a003573b56
jacobrya <zhan.nursadykov@gmail.com>
2026-01-19 23:50:43 +0500

    upd

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,31 +1,27 @@
 <x-app-layout>
-    <div class="py-12">
+    <div class="py-4">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
+                    <x-category-tabs>
+                        No categories
+                    </x-category-tabs>
+                </div>
+            </div>
+        </div>
+        <div class="max-w-5xl mx-auto sm:px-6 lg:px-8 mt-8">
+            <div class="mt-8 text-gray 900">
+                <div class="p-6 text-gray-900">
 
-                    <ul class="flex flex-wrap text-sm font-medium text-center text-gray-700 justify-center"> 
-                        <li class="me-2">
-                            <a href="#" class="inline-block px-4 py-2.5 text-white bg-blue-600 rounded-lg active"
-                                aria-current="page">Tab 1</a>
-                        </li>
-                        <li class="me-2">
-                            <a href="#"
-                                class="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100">Tab 2</a>
-                        </li>
-                        @foreach ($categories as $category)
-                         <li class="me-2">
-                            <a href="#"
-                                class="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100">{{ $category->name }}</a>
-                        </li>
+                    @forelse ($posts as $post)
+                        <x-post-item :post="$post"></x-post-item>
 
-                        @endforeach
-                        <li class="me-2">
-                            <a href="#"
-                                class="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100">Tab 3</a>
-                        </li>
-                    
+                    @empty
+                        <div class="text-center text-gray-400">No Posts Found</div>
 
+
+                    @endforelse
+                    {{ $posts->onEachSide(1)->links() }}
                 </div>
             </div>
         </div>
